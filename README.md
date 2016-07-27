d671fff6f24a0bc41a97df8a0be8fb131aed0121
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-07-27 10:06:53 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# iontorrent-backup
-Backup script for IonTorrent based on Rsync
+iontorrent-backup
+===================
+
+Backup shell script for IonTorrent based on Rsync
+- Script syncs results and archive directory to destination server. 
+- results directory deleted files are purged only on the first day of the month to be able to get a little bit of history.
+- archive directory is never purged, data should be kept in backup for atleast 6 years.
+
+
+Installation
+-------------
+On server: 
+- Create or use server used for destination.
+- Create user on server (ionbackup) and create SSH key pair + put public key in authorized_keys on server and private key at SSHKEYLOCATION
+On client: 
+- Copy files in logrotate.d to /etc/logrotate.d
+- Create directory /var/log/ionbackup
+- Create cronjob and add :  0 19 * * * /opt/ionbackup/ionbackup.sh or another time if you prefer.
+
+
+Parameters
+-------------
+Parameters are set in ionbackup.sh
+
+```
+THROTTLE=6400                                           # throttle max throughput in KB/s
+PIDFILE=/var/run/ionbackup.pid                          # PID file location
+DESTINATION=ionbackup@x.x.x.x:/data/ionbackup           # Server destination string replace x.x.x.x by DNS name or IP
+SSHKEYLOCATION=/opt/ionbackup/ionbackup.key             # Location of private key used for SSH connection to destination
+```
+
+
+Authors
+-------------
+Author Name <hugo.vanduijn@naturalis.nl>
---
+++ ionbackup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Rsync backup script for IONTorrent
+#
+
+# (Added by Atze)
+# Max troughput, to limit 'other' performance issues speed is in KB/s
+THROTTLE=6400
+PIDFILE=/var/run/ionbackup.pid
+DESTINATION=ionbackup@x.x.x.x:/data/ionbackup   # fill in IP of destionation server
+SSHKEYLOCATION=/opt/ionbackup/ionbackup.key
+
+
+if [ -e "$PIDFILE" ] ; then
+    # our pidfile exists, let's make sure the process is still running though
+    PID=`/bin/cat "$PIDFILE"`
+    if /bin/kill -0 "$PID" > /dev/null 2>&1 ; then
+        exit 0
+    fi
+ fi
+
+# create or update the pidfile
+/bin/echo "$$" > $PIDFILE
+
+# find out day of the month.
+monthday=$(date '+%d')
+
+# Backup archive location, does not delete any files at target location. ( should be kept atleast 5 years )
+rsync -e 'ssh -i $SSHKEYLOCATION' --bwlimit="$THROTTLE" -avz --timeout=300 --log-file="/var/log/ionbackup/rsyncbackup.log" /data/iontorrent/ $DESTINATION/archive/
+
+# Backup results location, delete file in destination every 1st day of the month.
+if [ "$monthday" = "01" ]; then
+  rsync -e 'ssh -i $SSHKEYLOCATION' --bwlimit="$THROTTLE" -avz --del --timeout=300 --log-file="/var/log/ionbackup/rsyncbackup.log" /results/ $DESTINATION/results/
+else
+  rsync -e 'ssh -i $SSHKEYLOCATION' --bwlimit="$THROTTLE" -avz --timeout=300 --log-file="/var/log/ionbackup/rsyncbackup.log" /results/ $DESTINATION/results/
+fi
+
+# remove PIDfile
+/bin/rm -f "$PIDFILE"
+
