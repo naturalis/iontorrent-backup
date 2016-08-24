b8719d0dd383949e9df183131243c45c3ea7dbfb
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2016-08-24 16:23:04 +0200

    added deletion of old files from archive

--- ionbackup.sh
+++ ionbackup.sh
@@ -9,7 +9,8 @@ THROTTLE=6400
 PIDFILE=/var/run/ionbackup.pid
 DESTINATION=ionbackup@x.x.x.x:/data/ionbackup   # fill in IP of destination server
 SSHKEYLOCATION=/opt/ionbackup/ionbackup.key
-
+# remove files older than 2 years from /data/iontorrent ( not from backup! )
+REMOVEFROMARCHIVE=730
 
 if [ -e "$PIDFILE" ] ; then
     # our pidfile exists, let's make sure the process is still running though
@@ -27,6 +28,9 @@ monthday=$(date '+%d')
 
 # Backup archive location, does not delete any files at target location. ( should be kept atleast 5 years )
 rsync -e 'ssh -i '$SSHKEYLOCATION --bwlimit="$THROTTLE" -avz --timeout=300 --log-file="/var/log/ionbackup/rsyncbackup.log" /data/iontorrent/ $DESTINATION/archive/
+
+# remove files older than REMOVEFROMARCHIVE days from /data/iontorrent
+find /data/iontorrent -type f -mtime +$REMOVEFROMARCHIVE -ls -exec rm -f -- {} \;
 
 # Backup results location, delete file in destination every 1st day of the month.
 if [ "$monthday" = "01" ]; then
